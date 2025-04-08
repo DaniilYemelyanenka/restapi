481a1aa79892e59ad242687171046d0a576b34df
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-04-08 22:15:02 +0300

    Initialize project

---
+++ src/main/java/com/yemelyanenka/restapi/controller/PetsPostController.java
@@ -0,0 +1,17 @@
+package com.yemelyanenka.restapi.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pets")
+public class PetsPostController {
+
+    @PostMapping
+    public void PetsPost(@RequestParam String name, Integer age){
+        System.out.println("name: " + name + "\n age: " + age);
+    }
+}
